a9a32a4abe71e49908c07c3ff66fd02371664bff
FelipeFranco97 <48889338+FelipeFranco97@users.noreply.github.com>
2023-09-25 12:21:51 -0500

    provisional design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+'use client'
+
 import styles from './page.module.scss'
+import { useState } from 'react'
 
 export default function Home() {
+  const [articleVisible, setArticleVisible] = useState(false)
+
+  const toggleArticle = () => {
+    setArticleVisible(!articleVisible)
+  }
+
+  const changeColor = {
+    backgroundColor: articleVisible ? '#D3D4D6' : '#222831'
+  }
   return (
     <>
       <section className={styles.section_main}>
         <h1 className={styles.h1_title}>¿Qué ruta elegirás hoy?</h1>
+
         <section className={styles.section_routes}>
           <article className={styles.article_hr_container}>
             <hr className={styles.hr_guide} />
@@ -13,11 +26,23 @@ export default function Home() {
           <article className={styles.article_main}>
             <article className={styles.article_route}>
               <div className={styles.div_empty}></div>
+
               <article className={styles.article_div_container}>
-                <div className={styles.div_circle}></div>
+                <div
+                  onClick={toggleArticle}
+                  className={styles.div_circle}
+                  style={changeColor}
+                ></div>
               </article>
-              <article className={styles.article_p_container}>
-                <p className={styles.p_title}>Introducción <br /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim fugit autem similique magni numquam quos iusto architecto provident nisi aliquam error impedit esse porro sunt, molestiae tempore eaque reprehenderit iure!</p>
+
+              <article className={`${styles.article_p_main} ${articleVisible ? styles.visible : styles.hidden}`}>
+                <article className={styles.article_p_container}>
+                  <h3>Introducción</h3>
+                  <p>
+                    Conceptos básicos antes de iniciar, ¿Qué es Web?, protocolos
+                    importantes, internet y más.
+                  </p>
+                </article>
               </article>
             </article>
           </article>

6bb1328c6c9c1e5c9a24378faa68c1fd569dfcff
FelipeFranco97 <48889338+FelipeFranco97@users.noreply.github.com>
2023-09-25 00:17:32 -0500

    root main design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,25 @@ import styles from './page.module.scss'
 export default function Home() {
   return (
     <>
-      <section>
+      <section className={styles.section_main}>
         <h1 className={styles.h1_title}>¿Qué ruta elegirás hoy?</h1>
-        <article className={styles.article_container_images}></article>
+        <section className={styles.section_routes}>
+          <article className={styles.article_hr_container}>
+            <hr className={styles.hr_guide} />
+          </article>
+
+          <article className={styles.article_main}>
+            <article className={styles.article_route}>
+              <div className={styles.div_empty}></div>
+              <article className={styles.article_div_container}>
+                <div className={styles.div_circle}></div>
+              </article>
+              <article className={styles.article_p_container}>
+                <p className={styles.p_title}>Introducción <br /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim fugit autem similique magni numquam quos iusto architecto provident nisi aliquam error impedit esse porro sunt, molestiae tempore eaque reprehenderit iure!</p>
+              </article>
+            </article>
+          </article>
+        </section>
       </section>
     </>
   )
